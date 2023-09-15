f6cff9bdd2bdf0dda75db9722277a16f5fef373f
aditi <sharmaaditi7505@gmail.com>
2023-09-15 14:28:31 +0530

    -

---
+++ if_in_js.js
@@ -0,0 +1,8 @@
+temp=20
+if(temp>30)
+{
+    console.log("its very cold outside")
+}
+else{
+console.log("ohk ohk")
+}
---
+++ intro.js
@@ -0,0 +1,5 @@
+var fullname="aditi sharma";
+var coursename="react js bootcamp";
+var loggedin=true;
+console.log(fullname,coursename,loggedin);
+console.log(fullname);
